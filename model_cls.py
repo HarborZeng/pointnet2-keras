588984894d1ee0c9c6bf698ee302d79ddc2830d3
harntu <harborzeng@qq.com>
2019-03-27 16:08:50 +0800

    update some code to make it work

--- model_cls.py
+++ model_cls.py
@@ -1,5 +1,5 @@
-from keras.layers import Conv2D, Flatten, Dropout, Input, BatchNormalization, Dense, InputLayer
-from keras.models import Model, Sequential
+from keras.layers import Conv2D, Flatten, Dropout, Input, BatchNormalization, Dense, Lambda
+from keras.models import Model
 from keras import backend as K
 from keras.engine.topology import Layer
 import numpy as np
@@ -21,45 +21,36 @@ def pointnet2(nb_classes):
                                               512,
                                               [0.1, 0.2, 0.4],
                                               [16, 32, 128],
-                                              [[32, 32, 64], [64, 64, 128], [64, 96, 128]])
+                                              [[32, 32, 64], [64, 64, 128], [64, 96, 128]],
+                                              use_nchw=True)
 
     sa2_xyz, sa2_points = set_abstraction_msg(sa1_xyz,
                                               sa1_points,
                                               128,
                                               [0.2, 0.4, 0.8],
                                               [32, 64, 128],
-                                              [[64, 64, 128], [128, 128, 256], [128, 128, 256]])
+                                              [[64, 64, 128], [128, 128, 256], [128, 128, 256]],
+                                              use_nchw=False)
 
     sa3_xyz, sa3_points = set_abstraction(sa2_xyz,
                                           sa2_points,
                                           [256, 512, 1024])
 
+    c = Lambda(lambda x: K.reshape(x, [16, -1]))(sa3_points)
     # point_net_cls
-    c = Dense(512, activation='relu')(sa3_points)
+    c = Dense(512, activation='relu')(c)
     c = BatchNormalization()(c)
     c = Dropout(0.5)(c)
     c = Dense(256, activation='relu')(c)
     c = BatchNormalization()(c)
     c = Dropout(0.5)(c)
-    c = Dense(nb_classes, activation='softmax')(c)
-    prediction = Flatten()(c)
-    output_keras = TfToKerasLayer()(prediction)
-    model = Model(inputs=model_input, outputs=output_keras)
-
-    # turn tf tensor to keras
+    prediction = Dense(nb_classes, activation='softmax')(c)
+    # turn tf tensor to keras model
+    model = Model(inputs=model_input, outputs=prediction)
     return model
 
 
-class TfToKerasLayer(Layer):
-    def __init__(self, **kwargs):
-        super(TfToKerasLayer, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        # 为该层创建一个可训练的权重
-        super(TfToKerasLayer, self).build(input_shape)  # 一定要在最后调用它
-
-
-def set_abstraction_msg(xyz, points, npoint, radius_list, nsample_list, mlp_list):
+def set_abstraction_msg(xyz, points, npoint, radius_list, nsample_list, mlp_list, use_nchw):
     new_xyz = gather_point(xyz, farthest_point_sample(npoint, xyz))
     new_points_list = []
     for i in range(len(radius_list)):
@@ -67,20 +58,20 @@ def set_abstraction_msg(xyz, points, npoint, radius_list, nsample_list, mlp_list
         nsample = nsample_list[i]
         group_idx = query_ball_point(radius, nsample, xyz, new_xyz)
         grouped_xyz = group_point(xyz, group_idx[0])
-        grouped_xyz -= tf.tile(tf.expand_dims(new_xyz, 2), [1, 1, nsample, 1])
+        grouped_xyz -= K.tile(Lambda(lambda x: K.expand_dims(x, axis=2))(new_xyz), [1, 1, nsample, 1])
         if points is not None:
             grouped_points = group_point(points, group_idx[0])
-            grouped_points = tf.concat([grouped_points, grouped_xyz], axis=-1)
+            grouped_points = Lambda(lambda x: K.concatenate(x, axis=-1))([grouped_points, grouped_xyz])
         else:
             grouped_points = grouped_xyz
-        grouped_points = tf.transpose(grouped_points, [0, 3, 1, 2])
+        if use_nchw: grouped_points = Lambda(lambda x: K.permute_dimensions(x, [0, 3, 1, 2]))(grouped_points)
         for j, num_out_channel in enumerate(mlp_list[i]):
             grouped_points = Conv2D(num_out_channel, 1, activation="relu")(grouped_points)
             grouped_points = BatchNormalization()(grouped_points)
-        grouped_points = tf.transpose(grouped_points, [0, 2, 3, 1])
-        new_points = tf.reduce_max(grouped_points, axis=[2])
+        if use_nchw: grouped_points = Lambda(lambda x: K.permute_dimensions(x, [0, 2, 3, 1]))(grouped_points)
+        new_points = Lambda(lambda x: K.max(x, axis=2))(grouped_points)
         new_points_list.append(new_points)
-    new_points_concat = tf.concat(new_points_list, axis=-1)
+    new_points_concat = Lambda(lambda x: K.concatenate(x, axis=-1))(new_points_list)
     return new_xyz, new_points_concat
 
 
@@ -89,54 +80,15 @@ def set_abstraction(xyz, points, mlp):
     new_xyz, new_points, idx, grouped_xyz = sample_and_group_all(xyz, points)
 
     # Point Feature Embedding
-    new_points = tf.transpose(new_points, [0, 3, 1, 2])
     for i, num_out_channel in enumerate(mlp):
         new_points = Conv2D(num_out_channel, 1, activation="relu")(new_points)
         new_points = BatchNormalization()(new_points)
-    new_points = tf.transpose(new_points, [0, 2, 3, 1])
 
     # Pooling in Local Regions
-    new_points = tf.reduce_max(new_points, axis=[2], keepdims=True, name='maxpool')
+    new_points = Lambda(lambda x: K.max(x, axis=2, keepdims=True))(new_points)
 
-    new_points = tf.squeeze(new_points, [2])  # (batch_size, npoints, mlp2[-1])
+    new_points = Lambda(lambda x: K.squeeze(x, 2))(new_points)  # (batch_size, npoints, mlp2[-1])
     return new_xyz, new_points
-
-
-def sample_and_group(npoint, radius, nsample, xyz, points, knn=False, use_xyz=True):
-    """
-    Input:
-        npoint: int32
-        radius: float32
-        nsample: int32
-        xyz: (batch_size, ndataset, 3) TF tensor
-        points: (batch_size, ndataset, channel) TF tensor, if None will just use xyz as points
-        knn: bool, if True use kNN instead of radius search
-        use_xyz: bool, if True concat XYZ with local point features, otherwise just use point features
-    Output:
-        new_xyz: (batch_size, npoint, 3) TF tensor
-        new_points: (batch_size, npoint, nsample, 3+channel) TF tensor
-        idx: (batch_size, npoint, nsample) TF tensor, indices of local points as in ndataset points
-        grouped_xyz: (batch_size, npoint, nsample, 3) TF tensor, normalized point XYZs
-            (subtracted by seed point XYZ) in local regions
-    """
-
-    new_xyz = gather_point(xyz, farthest_point_sample(npoint, xyz))  # (batch_size, npoint, 3)
-    if knn:
-        _, idx = knn_point(nsample, xyz, new_xyz)
-    else:
-        idx, pts_cnt = query_ball_point(radius, nsample, xyz, new_xyz)
-    grouped_xyz = group_point(xyz, idx)  # (batch_size, npoint, nsample, 3)
-    grouped_xyz -= tf.tile(tf.expand_dims(new_xyz, 2), [1, 1, nsample, 1])  # translation normalization
-    if points is not None:
-        grouped_points = group_point(points, idx)  # (batch_size, npoint, nsample, channel)
-        if use_xyz:
-            new_points = tf.concat([grouped_xyz, grouped_points], axis=-1)  # (batch_size, npoint, nample, 3+channel)
-        else:
-            new_points = grouped_points
-    else:
-        new_points = grouped_xyz
-
-    return new_xyz, new_points, idx, grouped_xyz
 
 
 def sample_and_group_all(xyz, points, use_xyz=True):
@@ -153,16 +105,15 @@ def sample_and_group_all(xyz, points, use_xyz=True):
     """
     batch_size = xyz.get_shape()[0].value
     nsample = xyz.get_shape()[1].value
-    new_xyz = tf.constant(np.tile(np.array([0, 0, 0]).reshape((1, 1, 3)), (batch_size, 1, 1)),
-                          dtype=tf.float32)  # (batch_size, 1, 3)
+    new_xyz = tf.constant(np.tile(np.array([0, 0, 0]).reshape((1, 1, 3)), (batch_size, 1, 1)), dtype=tf.float32)  # (batch_size, 1, 3)
     idx = tf.constant(np.tile(np.array(range(nsample)).reshape((1, 1, nsample)), (batch_size, 1, 1)))
-    grouped_xyz = tf.reshape(xyz, (batch_size, 1, nsample, 3))  # (batch_size, npoint=1, nsample, 3)
+    grouped_xyz = Lambda(lambda x: K.reshape(x, (batch_size, 1, nsample, 3)))(xyz)  # (batch_size, npoint=1, nsample, 3)
     if points is not None:
         if use_xyz:
-            new_points = tf.concat([xyz, points], axis=2)  # (batch_size, 16, 259)
+            new_points = Lambda(lambda x: K.concatenate(x, axis=2))([xyz, points])  # (batch_size, 16, 259)
         else:
             new_points = points
-        new_points = tf.expand_dims(new_points, 1)  # (batch_size, 1, 16, 259)
+        new_points = Lambda(lambda x: K.expand_dims(x, 1))(new_points)  # (batch_size, 1, 16, 259)
     else:
         new_points = grouped_xyz
     return new_xyz, new_points, idx, grouped_xyz
