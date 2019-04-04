911054a477afe2c13d3a4d1578b9e515a5ddb445
harbo <757772438@qq.com>
2019-04-04 22:16:40 +0800

    add summary

--- tf_cls.py
+++ tf_cls.py
@@ -2,11 +2,10 @@ import tensorflow as tf
 from model_cls import pointnet2
 import matplotlib
 
-from pointnet2_cls_msg import placeholder_inputs, get_loss, get_model
+from pointnet2_cls_msg import placeholder_inputs
 
 matplotlib.use('AGG')
 import matplotlib.pyplot as plt
-from data_loader import DataGenerator
 import os
 from keras import backend as K
 from modelnet_h5_dataset import ModelNetH5Dataset
@@ -14,21 +13,18 @@ import numpy as np
 
 nb_classes = 40
 
-train_file = './ModelNet40/ply_data_train.h5'
-test_file = './ModelNet40/ply_data_test.h5'
-
 epochs = 150
 batch_size = 16
 num_point = 1024
 
-EPOCH_CNT = 0
+decay_step = 200000
+bn_init_decay = 0.5
+bn_decay_decay_rate = 0.5
+bn_decay_decay_step = float(decay_step)
+bn_decay_clip = 0.99
+decay_rate = 0.7
 
-DECAY_STEP = 200000
-BN_INIT_DECAY = 0.5
-BN_DECAY_DECAY_RATE = 0.5
-BN_DECAY_DECAY_STEP = float(DECAY_STEP)
-BN_DECAY_CLIP = 0.99
-DECAY_RATE = 0.7
+summary_dir = 'summary'
 
 
 def plot_history(history, result_dir):
@@ -71,27 +67,27 @@ def save_history(history, result_dir):
 def get_learning_rate(batch):
     learning_rate = tf.train.exponential_decay(0.001,  # Base learning rate.
                                                batch * batch_size,  # Current index into the dataset.
-                                               DECAY_STEP,  # Decay step.
-                                               DECAY_RATE,  # Decay rate.
+                                               decay_step,  # Decay step.
+                                               decay_rate,  # Decay rate.
                                                staircase=True)
     learning_rate = tf.maximum(learning_rate, 0.00001)  # CLIP THE LEARNING RATE!
     return learning_rate
 
 
 def get_bn_decay(batch):
-    bn_momentum = tf.train.exponential_decay(BN_INIT_DECAY,
+    bn_momentum = tf.train.exponential_decay(bn_init_decay,
                                              batch * batch_size,
-                                             BN_DECAY_DECAY_STEP,
-                                             BN_DECAY_DECAY_RATE,
+                                             bn_decay_decay_step,
+                                             bn_decay_decay_rate,
                                              staircase=True)
-    bn_decay = tf.minimum(BN_DECAY_CLIP, 1 - bn_momentum)
+    bn_decay = tf.minimum(bn_decay_clip, 1 - bn_momentum)
     return bn_decay
 
 
 def train():
-    TRAIN_DATASET = ModelNetH5Dataset('./data/modelnet40_ply_hdf5_2048/train_files.txt',
+    train_dataset = ModelNetH5Dataset('./data/modelnet40_ply_hdf5_2048/train_files.txt',
                                       batch_size=batch_size, npoints=num_point, shuffle=True)
-    TEST_DATASET = ModelNetH5Dataset('data/modelnet40_ply_hdf5_2048/test_files.txt',
+    test_dataset = ModelNetH5Dataset('data/modelnet40_ply_hdf5_2048/test_files.txt',
                                      batch_size=batch_size, npoints=num_point, shuffle=False)
 
     point_cloud, labels = placeholder_inputs(batch_size, num_point)
@@ -114,45 +110,56 @@ def train():
     losses = tf.get_collection('losses')
     total_loss = tf.add_n(losses, name='total_loss')
     tf.summary.scalar('total_loss', total_loss)
-    for l in losses + [total_loss]:
-        tf.summary.scalar(l.op.name, l)
+    for the_lable in losses + [total_loss]:
+        tf.summary.scalar(the_lable.op.name, the_lable)
 
     correct = tf.equal(tf.argmax(pred, 1), tf.to_int64(labels))
     accuracy = tf.reduce_sum(tf.cast(correct, tf.float32)) / float(batch_size)
     tf.summary.scalar('accuracy', accuracy)
 
-    train_op = tf.train.AdamOptimizer(get_learning_rate(batch)).minimize(total_loss)
+    learning_rate = get_learning_rate(batch)
+    tf.summary.scalar('learning_rate', learning_rate)
+
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(total_loss, global_step=batch)
+
+    saver = tf.train.Saver()
+
+    session = K.get_session()
+
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter(os.path.join(summary_dir, 'train'), session.graph)
+    test_writer = tf.summary.FileWriter(os.path.join(summary_dir, 'test'), session.graph)
 
     init_op = tf.global_variables_initializer()
-    session = K.get_session()
     session.run(init_op)
 
     with session.as_default():
         with tf.device('/gpu:0'):
-            for i in range(epochs):
+            for epoch in range(epochs):
                 # TODO: add early stop to prevent overfitting
-                print('**** EPOCH %03d ****' % i)
+                print('**** EPOCH %03d ****' % epoch)
 
                 # Make sure batch data is of same size
-                cur_batch_data = np.zeros((batch_size, num_point, TRAIN_DATASET.num_channel()))
-                cur_batch_label = np.zeros((batch_size), dtype=np.int32)
+                cur_batch_data = np.zeros((batch_size, num_point, train_dataset.num_channel()))
+                cur_batch_label = np.zeros(batch_size, dtype=np.int32)
 
                 total_correct = 0
                 total_seen = 0
                 loss_sum = 0
                 batch_idx = 0
 
-                while TRAIN_DATASET.has_next_batch():
-                    batch_data, batch_label = TRAIN_DATASET.next_batch(augment=True)
+                while train_dataset.has_next_batch():
+                    batch_data, batch_label = train_dataset.next_batch(augment=True)
                     bsize = batch_data.shape[0]
                     cur_batch_data[0:bsize, ...] = batch_data
                     cur_batch_label[0:bsize] = batch_label
-                    _, loss_val, pred_val = session.run([train_op, total_loss, pred], feed_dict={
+                    _, loss_val, pred_val, summary, step = session.run([train_op, total_loss, pred, merged, batch], feed_dict={
                         point_cloud: cur_batch_data,
                         labels: cur_batch_label,
                         is_training_pl: True,
-                        batch: i
                     })
+
+                    train_writer.add_summary(summary, step)
 
                     pred_val = np.argmax(pred_val, 1)
                     correct = np.sum(pred_val[0:bsize] == batch_label[0:bsize])
@@ -167,11 +174,12 @@ def train():
                         total_seen = 0
                         loss_sum = 0
                     batch_idx += 1
-                TRAIN_DATASET.reset()
+
+                train_dataset.reset()
 
                 # Make sure batch data is of same size
-                cur_batch_data = np.zeros((batch_size, num_point, TEST_DATASET.num_channel()))
-                cur_batch_label = np.zeros((batch_size), dtype=np.int32)
+                cur_batch_data = np.zeros((batch_size, num_point, test_dataset.num_channel()))
+                cur_batch_label = np.zeros(batch_size, dtype=np.int32)
 
                 total_correct = 0
                 total_seen = 0
@@ -180,38 +188,44 @@ def train():
                 total_seen_class = [0 for _ in range(nb_classes)]
                 total_correct_class = [0 for _ in range(nb_classes)]
 
-                print('---- EPOCH %03d EVALUATION ----' % i)
+                print('---- EPOCH %03d EVALUATION ----' % epoch)
 
-                while TEST_DATASET.has_next_batch():
-                    batch_data, batch_label = TEST_DATASET.next_batch(augment=False)
+                while test_dataset.has_next_batch():
+                    batch_data, batch_label = test_dataset.next_batch(augment=False)
                     bsize = batch_data.shape[0]
                     # for the last batch in the epoch, the bsize:end are from last batch
                     cur_batch_data[0:bsize, ...] = batch_data
                     cur_batch_label[0:bsize] = batch_label
 
-                    _, loss_val, pred_val = session.run([train_op, total_loss, pred], feed_dict={
+                    _, loss_val, pred_val, summary, step = session.run([train_op, total_loss, pred, merged, batch], feed_dict={
                         point_cloud: cur_batch_data,
                         labels: cur_batch_label,
                         is_training_pl: False,
-                        batch: i
                     })
+
+                    test_writer.add_summary(summary, step)
+
                     pred_val = np.argmax(pred_val, 1)
                     correct = np.sum(pred_val[0:bsize] == batch_label[0:bsize])
                     total_correct += correct
                     total_seen += bsize
                     loss_sum += loss_val
                     batch_idx += 1
-                    for i in range(0, bsize):
-                        l = batch_label[i]
-                        total_seen_class[l] += 1
-                        total_correct_class[l] += (pred_val[i] == l)
+                    for bindex in range(0, bsize):
+                        the_lable = batch_label[bindex]
+                        total_seen_class[the_lable] += 1
+                        total_correct_class[the_lable] += (pred_val[bindex] == the_lable)
 
                 print('eval mean loss: %f' % (loss_sum / float(batch_idx)))
                 print('eval accuracy: %f' % (total_correct / float(total_seen)))
                 print('eval avg class acc: %f' % (
                     np.mean(np.array(total_correct_class) / np.array(total_seen_class, dtype=np.float))))
 
-                TEST_DATASET.reset()
+                test_dataset.reset()
+
+                if epoch % 10 == 0:
+                    save_path = saver.save(session, os.path.join(summary_dir, "model.ckpt"))
+                    print("Model saved in file: {}".format(save_path))
 
 
 if __name__ == '__main__':
