aeec9751ec31262fb6d597a7e30a5c4136ae5140
harbo <757772438@qq.com>
2019-04-07 17:34:20 +0800

    add eval py

--- test_cls.py
+++ test_cls.py
@@ -1,14 +1,69 @@
-import keras
-from model_cls import PointNet
-from data_loader import DataGenerator
+import tensorflow as tf
+from keras import backend as K
+import numpy as np
 
-nb_classes = 40
-test_file = './ModelNet40/ply_data_test.h5'
-epochs = 100
-batch_size = 32
-model = PointNet(nb_classes)
-val = DataGenerator(test_file, batch_size, nb_classes, train=False)
-model.load_weights("./results/pointnet.h5")
-loss, acc = model.evaluate_generator(val.generator(), verbose=1)
-print("loss is {}, and acc is {}".format(loss, acc))
+from modelnet_h5_dataset import ModelNetH5Dataset
+from tf_cls import batch_size, num_point, nb_classes, summary_dir
+from model_cls import pointnet2
+import os
 
+shuffle = False
+
+train_dataset = ModelNetH5Dataset('./data/modelnet40_ply_hdf5_2048/train_files.txt',
+                                  batch_size=batch_size, npoints=num_point, shuffle=shuffle)
+
+test_dataset = ModelNetH5Dataset('data/modelnet40_ply_hdf5_2048/test_files.txt',
+                                 batch_size=batch_size, npoints=num_point, shuffle=shuffle)
+
+point_cloud = K.placeholder(dtype=np.float32, shape=(batch_size, num_point, 3))
+labels = K.placeholder(dtype=np.int32, shape=batch_size)
+is_training_pl = K.placeholder(dtype=np.bool, shape=())
+
+logits = pointnet2(point_cloud, nb_classes, is_training_pl)
+top_k_op = tf.nn.in_top_k(logits, labels, 1)
+
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    ckpt = tf.train.get_checkpoint_state(os.path.join(summary_dir, "model.ckpt"))
+    if ckpt and ckpt.model_checkpoint_path:
+        # Restores from checkpoint
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    else:
+        print('No checkpoint file found')
+
+    cur_batch_data = np.zeros((batch_size, num_point, train_dataset.num_channel()))
+    cur_batch_label = np.zeros(batch_size, dtype=np.int32)
+    true_count = 0
+    total_batch_count = 0
+
+    while train_dataset.has_next_batch():
+
+        batch_data, batch_label = train_dataset.next_batch()
+        bsize = batch_data.shape[0]
+        total_batch_count += bsize
+        cur_batch_data[0:bsize, ...] = batch_data
+        cur_batch_label[0:bsize] = batch_label
+
+        preditions = sess.run([top_k_op])
+        true_count += np.sum(preditions)
+
+        precision = true_count / total_batch_count
+        print('train dataset cumulative precision is {}'.format(precision))
+
+    while test_dataset.has_next_batch():
+
+        batch_data, batch_label = train_dataset.next_batch()
+        bsize = batch_data.shape[0]
+        total_batch_count += bsize
+        cur_batch_data[0:bsize, ...] = batch_data
+        cur_batch_label[0:bsize] = batch_label
+
+        preditions = sess.run([top_k_op])
+        true_count += np.sum(preditions)
+
+        precision = true_count / total_batch_count
+        print('test dataset cumulative precision is {}'.format(precision))
+
+    train_dataset.reset()
+    test_dataset.reset()
